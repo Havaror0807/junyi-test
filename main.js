020c4d7bbadbbb6edc18a787edb88152ff466f1c
Havaror0807 <52322687+Havaror0807@users.noreply.github.com>
2019-12-12 14:44:42 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+    //1.
+    var hi = ["hello", "bonjour"];
+    var reserved = hi.reverse();
+    console.log(reserved);
+
+    var flip = "Greetings";
+    var fliped = flip.split('').reverse().join('');
+    console.log(fliped);
+    
+    //2.
+    
+    //3.
+    //我會選擇拿標有鉛筆的標示的袋子，因為題目有說標有鉛筆的一定不是只有裝鉛筆，就代表可以猜測是混合的袋子，剩餘兩個不是鉛筆就是原子筆。
+    
+    //4.
+    //不應該是用減掉三⼈各出 300 元 - 服務⽣還給他們⼀⼈ 30 元去算，這樣會少30元，變成一個人270自然而然就少錢了
